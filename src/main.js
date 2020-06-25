a636522e53e7cd8acc19238b9dcf6d2f47526558
Arsalan Fardi <arsalan.fardi@gmail.com>
2020-06-25 14:48:27 -0600

    Create footer template and make user video draggable

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-function consoleLog() {
-    console.log('Hello world!');
+import { dragElement } from '../modules/draggable.js';
+
+var video = document.querySelector("#videoElement");
+
+if (navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
 }
 
-
-
+dragElement(document.getElementById("videoElement"));
