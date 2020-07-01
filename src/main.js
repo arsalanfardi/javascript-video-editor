a99d82586bbfae56cf4336b1501c45279f621803
Arsalan Fardi <arsalan.fardi@gmail.com>
2020-07-01 00:42:12 -0600

    Complete timeline

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 import { dragElement } from '../modules/draggable.js';
 
-var video = document.querySelector("#videoElement");
+var video = document.querySelector('#videoElement');
 
 if (navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices.getUserMedia({ video: true })
     .then(function (stream) {
       video.srcObject = stream;
     })
-    .catch(function (err0r) {
+    .catch(function (error) {
       console.log("Something went wrong!");
     });
 }
 
-dragElement(document.getElementById("videoElement"));
+dragElement(document.getElementById('videoElement'));
+
+const container = document.getElementsByClassName('timeline')[0];
+const node = document.getElementById('timeline__element');
+for (let i=1; i<60; i++) {
+  let nodeClone = node.cloneNode(true);
+  nodeClone.querySelector('#timeline__seconds').innerHTML = i;
+  container.appendChild(nodeClone);
+}
