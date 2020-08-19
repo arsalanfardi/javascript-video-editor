5f3ed853825b7998aad5a07e714892614696b8b5
Arsalan Fardi <arsalan.fardi@gmail.com>
2020-08-19 00:37:44 -0600

    Implement video and audio selection in dropdown menu and persist when menu is closed

--- src/js/settings/settings.js
+++ src/js/settings/settings.js
@@ -1,5 +1,11 @@
 import { startUserVideo } from '../user-video/user-video.js';
 
+let currentStream;
+let constraints = { video: true, audio: true };
+let settingsVideo = document.querySelector('#settingsUserVideo');
+const audioSelect = document.querySelector('#audio-input');
+const videoSelect = document.querySelector('#video-input');
+
 /**
  * Binds the event listeners to the settings buttons
  */
@@ -13,9 +19,7 @@ export function initializeSettings() {
  * media options in selection menus
  */
 function openSettings() {
-  startUserVideo('#settingsUserVideo');
-  const audioSelect = document.querySelector('#audio-input');
-  const videoSelect = document.querySelector('#video-input');
+  getSettingsUserMedia(settingsVideo);
 
   // Remove the existing options in the dropdown menus to prevent duplication
   audioSelect.innerHTML = '';
@@ -28,19 +32,63 @@ function openSettings() {
   navigator.mediaDevices.enumerateDevices()
     .then(devices => {
       devices.forEach(device => {
-        var opt = document.createElement('option')
-        if (device.kind == 'audioinput') {
-          opt.appendChild(document.createTextNode(device.label))
+        let opt = document.createElement('option');
+        opt.value = device.deviceId;
+        if (device.kind === 'audioinput') {
+          opt.appendChild(document.createTextNode(device.label));
           audioSelect.appendChild(opt);
-        } else if (device.kind == 'videoinput') {
-          opt.appendChild(document.createTextNode(device.label))
+          opt.value === constraints.audio ? opt.selected = 'selected' : opt;
+        } else if (device.kind === 'videoinput') {
+          opt.appendChild(document.createTextNode(device.label));
           videoSelect.appendChild(opt);
+          opt.value === constraints.video ? opt.selected = 'selected' : opt;
         }
       });
+      audioSelect.onchange = changeAudioInput;
+      videoSelect.onchange = changeVideoSource;
     })
     .catch(function (err) {
       console.log(err.name + ": " + err.message);
     });
+}
+
+function getSettingsUserMedia() {
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(mediaStream => {
+        settingsVideo.srcObject = mediaStream;
+        currentStream = mediaStream;
+      })
+      .catch(function (error) {
+        console.log('Something went wrong!', error);
+      }
+    );
+  }
+}
+
+function changeAudioInput() {
+  if (typeof currentStream !== 'undefined') {
+    stopMediaTracks(currentStream);
+  }
+  constraints.audio = audioSelect.options[audioSelect.selectedIndex].value;
+  getSettingsUserMedia();
+}
+
+function changeVideoSource() {
+  if (typeof currentStream !== 'undefined') {
+    stopMediaTracks(currentStream);
+  }
+  constraints.video = videoSelect.options[videoSelect.selectedIndex].value;
+  getSettingsUserMedia();
+}
+
+/**
+ * Loops through each media track and stops them.
+ */
+function stopMediaTracks(stream) {
+  stream.getTracks().forEach(track => {
+    track.stop();
+  })
 }
 
 /**
