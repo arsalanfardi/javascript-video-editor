46fc0bcc32e294d31c47651025b1c20a9c807eef
Arsalan Fardi <arsalan.fardi@gmail.com>
2020-08-31 00:25:55 -0600

    Create custom file input menu

---
+++ src/js/upload/upload-video.js
@@ -0,0 +1,21 @@
+// import * as Dropzone from "dropzone/dist/dropzone.js";
+
+export function prepareUpload() {
+  // window.Dropzone = require('dropzone/dist/min/dropzone.min');
+  // Dropzone.options.dropzoneClass = {
+  //   autoProcessQueue: 'false', // Turns off automatic POST by dropzone
+  //   paramName: "file", // The name that will be used to transfer the file
+  //   acceptedFiles: 'video/*',
+  //   maxFilesize: 1, // MB
+  //   url: '/file-upload',
+  //   init: function() {
+  //     this.on("drop", function(file) { alert("Added file."); });
+  //   },
+  //   accept: function(file, done) {
+  //     if (file.name == "justinbieber.jpg") {
+  //       done("Naha, you don't.");
+  //     }
+  //     else { done(); }
+  //   }
+  // };
+}
